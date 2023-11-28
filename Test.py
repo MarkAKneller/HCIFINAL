c263e9993f875df561bf32a1c175ca024aeefbb8
paradox <wallermichael14@gmail.com>
2023-11-28 15:06:32 -0500

    Changed API key

--- Test.py
+++ Test.py
@@ -9,20 +9,26 @@ if 'data' not in st.session_state:
     st.session_state['data'] = {}
 
 # Alpha Vantage API Key and Base URL
-API_KEY = 'IBR8NA5DH28CVSUP'
-BASE_URL = "https://www.alphavantage.co/query"
+url = "https://alpha-vantage.p.rapidapi.com/query"
+headers = {
+    "X-RapidAPI-Key": "7300c21118mshb9b8940b597a52cp1d3136jsnd9f0c95239bd",
+    "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+}
+
 
 # Function to fetch cryptocurrency data
 def get_crypto_data(symbol, market):
-    params = {
-        'function': 'DIGITAL_CURRENCY_DAILY',
-        'symbol': symbol,
+    querystring = {
         'market': market,
-        'apikey': API_KEY
+        'function': 'DIGITAL_CURRENCY_DAILY',
+        "symbol": symbol
     }
-    response = requests.get(BASE_URL, params=params)
+
+    response = requests.get(url, headers=headers, params=querystring)
     data = response.json()
+    print(response.json())
     return data
+
 
 # Set up the page
 st.title('Cryptocurrency Analysis Dashboard')
@@ -30,8 +36,10 @@ st.write("Explore the dynamic world of cryptocurrencies. Analyze daily trends, p
 
 # Sidebar for user input
 st.sidebar.header('Customize Your Analysis')
-symbol = st.sidebar.selectbox('Cryptocurrency Symbol', ['BTC', 'SOL', 'ETC'], help='Enter a cryptocurrency symbol (e.g., BTC, ETH)')
-market = st.sidebar.selectbox('Market Currency', ['USD', 'EUR', 'JPY'], help='Select the currency for market comparison')
+symbol = st.sidebar.selectbox('Cryptocurrency Symbol', ['BTC', 'SOL', 'ETC'],
+                              help='Enter a cryptocurrency symbol (e.g., BTC, ETH)')
+market = st.sidebar.selectbox('Market Currency', ['USD', 'EUR', 'JPY'],
+                              help='Select the currency for market comparison')
 
 # Fetch data button
 if st.sidebar.button('Analyze'):
@@ -39,7 +47,8 @@ if st.sidebar.button('Analyze'):
         st.session_state['data'] = get_crypto_data(symbol, market)
         if 'Time Series (Digital Currency Daily)' in st.session_state['data']:
             st.success(f'Data for {symbol} successfully retrieved!')
-            st.session_state['df'] = pd.DataFrame.from_dict(st.session_state['data']['Time Series (Digital Currency Daily)'], orient='index')
+            st.session_state['df'] = pd.DataFrame.from_dict(
+                st.session_state['data']['Time Series (Digital Currency Daily)'], orient='index')
             st.session_state['df'] = st.session_state['df'].apply(pd.to_numeric)
             st.subheader(f'Daily Data for {symbol} in {market}')
             st.dataframe(st.session_state['df'].head())
